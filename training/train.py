669dd68f7d256cae0147b4a5ead75ff95ec996ca
AndersonDaniel <daniel.anderson.d@gmail.com>
2022-08-05 12:00:12 +0300

    updates

--- training/train.py
+++ training/train.py
@@ -7,20 +7,25 @@ import json
 from tqdm import tqdm
 
 
-N_EPOCHS = 200
+N_EPOCHS = 2000
+WINDOW_SIZE = 8
+MINI_WINDOW_SIZE = 12
 
 
 def main():
     with open('data/metadata.json', 'r') as f:
         metadata = json.load(f)
 
-    ds = SensorFusionDataset('data/preprocessed/pamap2/105.npy')
+    ds = SensorFusionDataset('data/preprocessed/pamap2/101.npy', window_size=WINDOW_SIZE,
+                             mini_window_size=MINI_WINDOW_SIZE)
     # ds = SensorFusionDataset('data/preprocessed/skoda/left.npy')
     dl = DataLoader(ds, shuffle=True, batch_size=64)
-    model = AttnSense(metadata['pamap2']['sensor_groups'], ds.labels.shape[1], ds.n_freq)
+    model = AttnSense(metadata['pamap2']['sensor_groups'], ds.labels.shape[1], ds.n_freq,
+                      window_size=WINDOW_SIZE, mini_window_size=MINI_WINDOW_SIZE)
     # model = AttnSense(metadata['skoda']['sensor_groups'], ds.labels.shape[1])
     loss_f = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=1e-4)
     for epoch in range(1, N_EPOCHS + 1):
         pbar = tqdm(iter(dl), desc=f'Epoch {epoch}/{N_EPOCHS}')
         losses = []
